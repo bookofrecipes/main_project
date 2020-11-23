bc865a7477eefce4aee8e346c3ac8d3753bc3127
AGulyayev <adgulyaev@gmail.com>
2020-11-23 11:56:30 +0300

    Init repo.

---
+++ app/src/main/java/ru/geekbrains/bookofrecipes/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.geekbrains.bookofrecipes.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.geekbrains.bookofrecipes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
