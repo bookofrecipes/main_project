8f88308c17ad2a8e7aae3190eba2fc9f38a44ed5
mvrlrd <igrbelykh@gmail.com>
2020-11-24 14:48:18 +0300

    added DataSource interface, created methods for querying data from the server in SpoonacularApiService

---
+++ app/src/main/java/ru/geekbrains/bookofrecipes/data/network/DataSource.kt
@@ -0,0 +1,12 @@
+package ru.geekbrains.bookofrecipes.data.network
+
+import retrofit2.Response
+import ru.geekbrains.bookofrecipes.data.response.RandomRecipesResponse
+import ru.geekbrains.bookofrecipes.data.response.RecipeInformationResponse
+import ru.geekbrains.bookofrecipes.data.response.RecipesByIngredientsResponse
+
+interface DataSource {
+    suspend fun getData(quantityOfRandom : Int): Response<RandomRecipesResponse>
+    suspend fun getData(id : Long): Response<RecipeInformationResponse>
+    suspend fun getData(ingredients: String): Response<RecipesByIngredientsResponse>
+}
