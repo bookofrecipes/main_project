2b1974e5656abb3d7d45450c66ed8902bf101b31
alexander <Mkedonsky>
2020-11-25 14:16:48 +0300

    добавил экраны

---
+++ app/src/main/java/ru/geekbrains/bookofrecipes/presentation/ui/recipes/RecipesFragment.kt
@@ -0,0 +1,31 @@
+package ru.mkedonsky.bookofrecipes.ui.recipes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ru.geekbrains.bookofrecipes.R
+
+class RecipesFragment : Fragment() {
+
+    private lateinit var recipesViewModel: RecipesViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        recipesViewModel =
+                ViewModelProvider(this).get(RecipesViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_recipes, container, false)
+        val textView: TextView = root.findViewById(R.id.text_recipes)
+        recipesViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
