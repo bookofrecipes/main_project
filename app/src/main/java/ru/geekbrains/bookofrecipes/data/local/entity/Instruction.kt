4f4614492bda918c92ab200217056880c0825aea
mvrlrd <igrbelykh@gmail.com>
2020-12-19 21:37:15 +0300

    done with steps

--- app/src/main/java/ru/geekbrains/bookofrecipes/data/local/entity/Instruction.kt
+++ app/src/main/java/ru/geekbrains/bookofrecipes/data/local/entity/Instruction.kt
@@ -1,13 +1,18 @@
 package ru.geekbrains.bookofrecipes.data.local.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+
 
 data class Instruction(
     @SerializedName("steps")
     val stepList: List<Step>
 )
 
+@Entity
 data class Step(
+    @PrimaryKey(autoGenerate = false)
     @SerializedName("number")
     val number: Int,
     @SerializedName("step")
--- app/src/main/java/ru/geekbrains/bookofrecipes/data/local/entity/Nutrition.kt
+++ app/src/main/java/ru/geekbrains/bookofrecipes/data/local/entity/Nutrition.kt
@@ -1,5 +1,7 @@
 package ru.geekbrains.bookofrecipes.data.local.entity
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 data class Nutrition(
@@ -7,12 +9,14 @@ data class Nutrition(
     val nutrients: List<Nutrient>
 )
 
+@Entity
 data class Nutrient(
     @SerializedName("amount")
     val amount: Double?,
     @SerializedName("percentOfDailyNeeds")
     val percentOfDailyNeeds: Double?,
     @SerializedName("title")
+    @PrimaryKey(autoGenerate = false)
     val title: String?,
     @SerializedName("unit")
     val unit: String?
