96b02e5ff8499e8ca958a85a8a78d0b834c6050c
Aleksandr Gulyaev <adgulyaev@gmail.com>
2020-12-22 00:10:30 +0300

    Исправил косяк с Glide, при котором он не находил какой-то служебный класс.

--- app/src/main/java/ru/geekbrains/bookofrecipes/service/extensions/View.kt
+++ app/src/main/java/ru/geekbrains/bookofrecipes/service/extensions/View.kt
@@ -6,10 +6,15 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.annotation.LayoutRes
 import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.module.AppGlideModule
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View =
     LayoutInflater.from(context).inflate(layoutRes, this, false)
+
+@GlideModule
+class MyGlideModule : AppGlideModule()
 
 fun ImageView.loadFromUrl(url: String?) =
     Glide.with(this.context.applicationContext)
